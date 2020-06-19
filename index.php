17520899b332c25b7076d781a95f0ddccd895fde
Shalom <shalomjsph@gmail.com>
2020-06-19 14:43:15 +0100

    airtime api integration

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,14 +12,25 @@
     <script src="https://use.fontawesome.com/5b1ba0a395.js"></script>
 </head>
 <body>
+
+   
     <header><h1><i class="fa fa-bolt" aria-hidden="true"> </i> lightningTops</h1></header>
     <section>
+
+    <?php  
+         if(isset($_SESSION['error1']) && !empty($_SESSION['error1'])) {
+             echo "sshi";
+             
+         }
+    
+    ?>
         <p>Top up your Naija network with the speed of <i class="fa fa-bolt" aria-hidden="true"></i>ight</p>
-        <form method="GET">
+        <form method="POST" action="process.php">
+
             <label for="number">Phone Number</label>
-            <input type="text" name="Phone number" placeholder="+2348150455581" id="number">
+            <input type="text" name="Phone_number" placeholder="+2348150455581" id="number">
             <label for="Network Provider" >Network Provider</label>
-            <select name="Network Provider" id="Network Provider" >
+            <select name="Network_Provider" id="Network Provider" >
                 <option value="MTN">MTN</option>
                 <option value="9mobile">9mobile</option>
                 <option value="Airtel">Airtel</option>
---
+++ process.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+$error1 = 0;
+	  
+$amount = $_POST["Amount"] != ""? $_POST["Amount"] : $error1++ ;
+$phoneNumber = $_POST["Phone_number"] != ""? $_POST["Phone_number"] : $error1++ ;
+$code = $_POST["Network_Provider"] != ""? $_POST["Network_Provider"] : $error1++ ;
+
+
+if($error1 > 0){
+	$_SESSION['error1']= "please fill all fields";
+	header("Location: index.php");
+} 
+
+$secretkey="l0hc6jc71yyy ";
+$publickey = "dotg8wddhzz6";
+
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://app.wallet.africa/bills/airtime/purchase",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS =>"{\r\n  \"Code\": $code,\r\n  \"Amount\": $amount,\r\n  \"PhoneNumber\": $phoneNumber,\r\n  \"SecretKey\": $secretkey \r\n}",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer <$publickey>",
+    "Content-Type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+
+?>
